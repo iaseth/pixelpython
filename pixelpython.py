5770a16ae717a7ce10d62b8d0ec6fa6a2dc66d77
Ankur Seth <iankurseth@gmail.com>
2025-02-02 15:51:23 +0530

    Moved utils inside pypixel module

--- pixelpython.py
+++ pixelpython.py
@@ -1,24 +1,9 @@
-import json
 import random
 import sys
 
 from PIL import Image, ImageDraw
+from pypixel.utils import get_rgb_components
 
-
-
-with open('colors.json') as f:
-	colors_json = json.load(f)
-	colors = colors_json['colors']
-
-
-def get_rgb_components(color_string):
-	if color_string.startswith("#"):
-		color_string = color_string.lstrip("#")
-		return tuple(int(color_string[i:i+2], 16) for i in (0, 2, 4))
-	else:
-		for color in colors:
-			if color['name'].upper() == color_string.upper():
-				return get_rgb_components(color['code'])
 
 
 def generate_wallpaper(
